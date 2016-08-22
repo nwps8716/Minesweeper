f96aef35a567ba0a1649b08dab3968d51800a2de
Lid <nwps8716@gmail.com>
2016-08-22 10:03:54 +0000

    修改版本

--- Minesweeper2.php
+++ Minesweeper2.php
@@ -1,43 +1,28 @@
 <?php
-//地圖
-for ($x = 0 ; $x < 50 ; $x++)
-{
-    for($y = 0 ; $y < 60 ; $y++)
-    {
-        $map[$x][$y] = "?";
-    }
-}
 
-//亂數炸彈位置
-for ($i = 0 ; $i < 1200 ; $i++)
+for ($i=0 ; $i<3000 ; $i++)
 {
-    $m = rand(0,2999);
-    for ($j = 0 ; $j < count($bomb) ; $j++)
-    {
-        if($bomb[$j] == $m)
-        {
-            $i--;
-            continue 2;
-        }
-    }
-    $bomb[] = $m;
+    $bomb[] = $i + 1;
 }
+shuffle($bomb);
 
-//拆解成x,y位置,放入地圖
-for ($i = 0 ; $i < 1200 ; $i++)
-{
-    $temp = $bomb[$i];
-    $x = floor($temp/60);
-    $y = $temp % 60;
-    $map[$x][$y] = "M";
-}
-
-//判斷八個方位的數字
 for ($x = 0 ; $x < 50 ; $x++)
 {
     for ($y = 0 ; $y < 60 ; $y++)
     {
-        if($map[$x][$y] === "?")
+        if ($bomb[$x * 60 + $y] <= 1200)
+        {
+            $bomb[$x * 60 + $y] = "M";
+        }
+        $map[$x][$y] = $bomb[$x * 60 + $y];
+    }
+}
+
+for ($x = 0 ; $x < 50 ; $x++)
+{
+    for ($y = 0 ; $y < 60 ; $y++)
+    {
+        if($map[$x][$y] !== "M")
         {
             $count = 0;
             if($map[$x-1][$y-1] === "M")
@@ -88,7 +73,9 @@ for ($x = 0 ; $x < 50 ; $x++)
         }
     }
 }
-
 echo $data;
+
+
+
 
 ?>
